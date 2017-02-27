330bd988fc8af649d32656145e6e11123671a2f5
Jan Espen Kleivane <espenkleivane@gmail.com>
2017-02-27 22:53:41 +0100

    started consumer

--- Communication/Communication/Recieve/Consumer.cs
+++ Communication/Communication/Recieve/Consumer.cs
@@ -3,10 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Exceptions;
+using RabbitMQ.Client.Events;
+using Communication.Amqp;
 
 namespace Communication.Recieve
 {
-    class Consumer
+    class Consumer : AmqpMessagingService
     {
+
+        public Consumer()
+        {
+
+        }
+
+        public string recieveMsg(IModel mod, string queue)
+        {
+            string msg = "";
+            var reciever = new EventingBasicConsumer(mod);
+            reciever.Received += (model, ea) =>
+            {
+                var body = ea.Body;
+                var message = Encoding.UTF8.GetString(body);
+                msg = "Recieved " + message;
+            };
+            mod.BasicConsume(queue, noAck: true, consumer: reciever);
+
+
+            return msg;
+        }
     }
 }
