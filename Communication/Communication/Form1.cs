2d57e76054a2e0ad9d74c9a5f555c189a5ed42aa
Jan Espen Kleivane <espenkleivane@gmail.com>
2017-02-27 21:21:23 +0100

    27.02.2017 time21:21

--- Communication/Communication/Form1.cs
+++ Communication/Communication/Form1.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using Communication.Utility;
 using Communication.Send;
+using RabbitMQ.Client;
 
 namespace Communication
 {
@@ -21,12 +22,57 @@ namespace Communication
 
         private void buttonSend_Click(object sender, EventArgs e)
         {
-            string send = textBoxSend.Text;
-            Helper.followTextBoxLog(richTextBoxLogs, send);
-            //Helper.followTextBoxLog(richTextBoxLogs, Sender.test());
-          
+            int start = 1;
+            int work = 2;
+            int reTry = 3;
+           
+            Producer producer = new Producer();
+            int state = start;
+            if (state == start)
+            {
+
+                bool status = producer.getRabbitMqConnection();
+                Helper.followTextBoxLog(richTextBoxLogs, "Connection is " + status);
+                if (status == true)
+                {
+                    state = work;
+
+                    if (state == work)
+                    {
+                        Helper.followTextBoxLog(richTextBoxLogs, "State is work " + state);
+                        string msg = textBoxSend.Text;
+                        if (msg.Length < 5)
+                        {
+                            Helper.followTextBoxLog(richTextBoxLogs, "Enter a new messages, 5 chars it to small");
+                        }
+                        else
+                        {
+                            string sent = producer.publishMsg(msg);
+                            Helper.followTextBoxLog(richTextBoxLogs, sent);
+                            textBoxSend.Text = "";
+                        }
+                    }
+                }
+                else
+                {
+                    state = reTry;
+                    producer.reConnectToRabbit();
+                    string info = "State is retry " + state;
+                    Helper.followTextBoxLog(richTextBoxLogs, info);
+                    if (producer.getRabbitMqConnection() == true)
+                    {
+                        state = work;
+                        Helper.followTextBoxLog(richTextBoxLogs, "Reconnect is ok, state is work " + state);
+                    }
+
+                }
+
+            }
 
         }
+
+
+
 
         private void buttonReceive_Click(object sender, EventArgs e)
         {
---
+++ Communication/Communication/Send/Producer.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Exceptions;
+using System.IO;
+
+
+namespace Communication.Send
+{
+    class Producer
+    {
+        private ConnectionFactory conFac { get; set; }
+        private IModel model { get; set; }
+        private IConnection con { get; set; }
+        private bool connectionStatus;
+
+
+        public Producer()
+        {
+            connectionStatus = false;
+        }
+
+        public bool getRabbitMqConnection()
+        {
+
+            string info = "";
+            try
+            {
+               
+                    //connection
+                    conFac = new ConnectionFactory();
+                    conFac.HostName = "localhost";
+                    conFac.UserName = "guest";
+                    conFac.Password = "guest";
+                    //automatic reconnect
+                    conFac.AutomaticRecoveryEnabled = true;
+                    //conFac.NetworkRecoveryInterval = TimeSpan.FromSeconds(20);
+                    con = conFac.CreateConnection();
+                    setUpInitialTopicQueue();
+                    connectionStatus = true;
+                    info = "Success";
+
+            }
+
+            catch (BrokerUnreachableException msg)
+            {
+                info = " " + msg;
+                connectionStatus = false;
+            }
+            catch (Exception msg)
+            {
+                info = "" + msg;
+                connectionStatus = false;
+            }
+
+
+            return connectionStatus;
+
+        }
+        public void reConnectToRabbit()
+        {
+            conFac.NetworkRecoveryInterval = TimeSpan.FromSeconds(10);
+            connectionStatus = false;
+
+        }
+        private void setUpInitialTopicQueue()
+        {
+            model = con.CreateModel();
+            //create queue:name, durable, exclusive, autodelte, arguments
+            //Durability: durable(meaning messages can be recovered)
+            model.QueueDeclare("oil_2", true, false, false, null);
+            //create exchange
+            //Topic exchanges route messages to one or many queues based on matching between a message routing key and the pattern that was used to bind a queue to an exchange. 
+            model.ExchangeDeclare("to_oil_2", ExchangeType.Topic);
+            //bind the them with routing key
+            model.QueueBind("oil_2", "to_oil_2", "values");
+        }
+
+        public string publishMsg(string messages)
+        {
+            string info = "";
+            try
+            {
+                IBasicProperties basicProp = model.CreateBasicProperties();
+                basicProp.Persistent = false;
+                //msg
+                byte[] load = Encoding.UTF8.GetBytes(messages);
+                PublicationAddress adr = new PublicationAddress(ExchangeType.Topic, "to_oil_2", "values");
+                model.BasicPublish(adr, basicProp, load);
+                info = messages;
+            }
+            catch (NullReferenceException msg)
+            {
+                info = "" + msg;
+            }
+
+            return "Publishes msg " + info;
+        }
+
+    }
+}
